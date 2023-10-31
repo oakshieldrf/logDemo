7496a5edde02ccd32819ccbbff48e1bda3d7ba62
fernando <fernandrobroy@gmail.com>
2023-10-31 09:34:20 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,25 @@
+class Calculator {
+    add(a,b){
+        return a+b;
+    }
+
+    multiply(a,b) {
+        return a*b;
+    }
+
+    division(a,b){
+        return a/b;
+    }
+
+    subtract(a,b){
+        return a-b;
+    }
+
+    potencia(a,b){
+        return Math.pow(a,b);
+    }
+
+    modulo(a,b) {
+        return a%b;
+    }
+}
